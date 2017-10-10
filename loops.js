8ef9786507eb17476e45fcdd88008dd72df9e5fa
Andy Striph <ajstriph@me.com>
2017-10-10 23:07:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,6 +3,6 @@ for (i === 0; i < 25; i++){
     console.log("I am 1 strange loop.");
   }
   else {
-    console.log("I am ${i} strange loops.");
+    console.log("I am ${i}'s strange loops.");
   }
 }
